494eedfbd97c299ef40d8b717509ba369a3033b9
Salim Al Mersally <salim.almersally@lau.edu>
2021-06-16 12:18:48 +0300

    Create Wallet Form done and linked to stateProvider

--- src/StateProvider.js
+++ src/StateProvider.js
@@ -1,31 +1,33 @@
-import React, { createContext, useReducer } from 'react'
+import React, { createContext, useReducer } from "react";
 
-export const WalletContext = createContext()
+export const WalletContext = createContext();
 
 const initialState = {
   wallets: [],
-  currentWalletID: '',
+  currentWalletID: "",
   currentTransaction: {},
-}
+};
 
 function reducer(state, action) {
   switch (action.type) {
-    case 'SET_WALLETS':
-      return { ...state, wallet: action.value }
-    case 'SET_WALLETID':
-      return { ...state, currentWalletID: action.value }
-    case 'SET_CURRENTTRANS':
-      return { ...state, currentTransaction: action.value }
+    case "SET_WALLET":
+      return { ...state, wallets: [...state.wallets, action.value] };
+    case "SET_WALLETS":
+      return { ...state, wallets: action.value };
+    case "SET_WALLETID":
+      return { ...state, currentWalletID: action.value };
+    case "SET_CURRENTTRANS":
+      return { ...state, currentTransaction: action.value };
     default:
-      return state
+      return state;
   }
 }
 export function StateProvider(props) {
-  const [state, dispatch] = useReducer(reducer, initialState)
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <WalletContext.Provider value={[state, dispatch]}>
       {props.children}
     </WalletContext.Provider>
-  )
+  );
 }
--- src/components/main/noWalletsErrorPage.jsx
+++ src/components/main/noWalletsErrorPage.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import WalletForm from "./createWalletForm";
 
-function noWalletsErrorPage() {
+function NoWalletsErrorPage() {
+  const [showForm, SetShowForm] = useState(false);
+
+  function createWallet() {
+    SetShowForm(true);
+  }
+
   return (
-    <div>
+    <div className="text-center">
       <h1>TRUST WALLET</h1>
-
       <h2>OOops</h2>
       <h4>You have no Wallets... start by creating one</h4>
+      <button
+        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded-full"
+        onClick={createWallet}
+      >
+        Create form
+      </button>
+      {showForm && <WalletForm />}
     </div>
   );
 }
 
-export default noWalletsErrorPage;
+export default NoWalletsErrorPage;
